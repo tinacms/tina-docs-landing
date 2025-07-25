c05e757b2e6abf7bce6d40f90f3727e71b8c69dd
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-07-25 12:22:55 +1000

    adding seo related schemas and default collection

---
+++ src/utils/format-excerpt.ts
@@ -0,0 +1,53 @@
+interface TextNode {
+    type: string;
+    text: string;
+  }
+  
+  interface LinkNode {
+    type: string;
+    children: TextNode[];
+  }
+  
+  
+  interface ContentBody {
+    children: any[];
+  }
+
+export const formatExcerpt = (
+    body: ContentBody,
+    excerptLength: number
+  ): string => {
+    return body.children
+      ?.filter((c: any) => c.type === "p")
+      ?.reduce((excerpt: string, child: any) => {
+        // Combine all of child's text and link nodes into a single string
+        const paragraphText = child.children
+          .filter((c: any) => c.type === "text" || c.type === "a")
+          .reduce((text: string, child: TextNode | LinkNode) => {
+            if (child.type === "text") {
+              return text + (text ? " " : "") + (child as TextNode).text;
+            }
+            if (child.type === "a") {
+              return (
+                text +
+                (text ? " " : "") +
+                (child as LinkNode).children.map((c) => c.text).join(" ")
+              );
+            }
+            return text;
+          }, "");
+  
+        // Add paragraph to excerpt with space separator
+        const updatedExcerpt = excerpt
+          ? `${excerpt} ${paragraphText}`
+          : paragraphText;
+  
+        // Truncate if the combined text is too long
+        if (updatedExcerpt.length > excerptLength) {
+          return `${updatedExcerpt.substring(0, excerptLength)}...`;
+        }
+  
+        return updatedExcerpt;
+      }, "");
+  };
+  

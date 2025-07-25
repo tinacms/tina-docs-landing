b4bd610906ca06dcb4b3ea6ff58d688461949ede
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-07-25 12:34:48 +1000

    adding next-sitemap package

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://tina.io",
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 5000,

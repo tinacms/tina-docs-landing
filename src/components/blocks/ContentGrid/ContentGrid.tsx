d311b6702c1f4f490573cc68a45ac5622f9cd08b
joshbermanssw <joshberman@ssw.com.au>
2025-07-31 11:19:57 +1000

    key feet

--- src/components/blocks/ContentGrid/ContentGrid.tsx
+++ src/components/blocks/ContentGrid/ContentGrid.tsx
@@ -3,6 +3,7 @@ import { Section } from "../../layout/section";
 import Link from "next/link";
 import { tinaField } from "tinacms/dist/react";
 import { ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
 
 type ContentGridItem = {
   title: string;
@@ -29,27 +30,39 @@ const ContentGridItem = ({
       <h3 className="text-3xl font-bold">{item.title}</h3>
       <TinaMarkdown content={item.subtext} />
       {item.links && (
-              <Link
-                href={item.links.url}
-                data-tina-field={tinaField(item, "links")}
-                className="inline-flex items-center text-[#FF9B73] hover:text-[#d28161] font-medium transition-colors group"
-              >
-                Learn more
-                <ChevronRightIcon className="w-5 h-5 ml-1 group-hover:translate-x-1 transition-transform" />
-              </Link>
-            )}
+        <Link
+          href={item.links.url}
+          data-tina-field={tinaField(item, "links")}
+          className="inline-flex items-center text-[#FF9B73] hover:text-[#d28161] font-medium transition-colors group"
+        >
+          Learn more
+          <ChevronRightIcon className="w-5 h-5 ml-1 group-hover:translate-x-1 transition-transform" />
+        </Link>
+      )}
     </div>
   );
 };
 
 export default function ContentGrid({ data }: { data: any }) {
+  console.log("content grid", data);
   return (
     <Section className="flex flex-col gap-16 items-center justify-center">
       <h2 className="text-5xl font-bold">{data.title}</h2>
-      <div className="grid grid-cols-3 w-full ">
+      <div className="relative grid grid-cols-3 w-full h-[750px]">
         {data.itemList.map((item: any, index: number) => (
           <ContentGridItem key={index} item={item} onlyYBorder={index === 1} />
         ))}
+        {data.image && (
+          <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[60%] aspect-[16/9]">
+            <Image
+              src={data.image}
+              alt=""
+              fill
+              className="object-cover rounded-t-lg"
+              data-tina-field={tinaField(data, "image")}
+            />
+          </div>
+        )}
       </div>
     </Section>
   );
